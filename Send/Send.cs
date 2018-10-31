b43121b50d50597d963b23c1b132465c8835088a
Glenn Spinks <glennspinks@gmail.com>
2018-10-31 21:19:46 +0800

    Basic publish subscribe

---
+++ Send/Send.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+
+            do
+            {
+                result = SendMessage();
+
+            } while (result == "s");
+        }
+
+        static string SendMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "spinksy", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    string message = "Hello from Spinksy";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "spinksy", basicProperties: null, body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine("\n Press [s] to send another message\n Press [enter] to exit.");
+            return Console.ReadLine();
+        }
+    }
+}
